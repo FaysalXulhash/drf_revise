dc5f586e418eda4d6e2284ac073d6655aeaaac05
FaysalXulhash <faysalahammed80@gmail.com>
2024-10-08 22:20:02 +0600

    implemented deserialization

--- myapp/serializars.py
+++ myapp/serializars.py
@@ -6,3 +6,6 @@ class PersonSerializar(serializers.Serializer):
 
     name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,10 @@ from.models import Person
 from .serializars import PersonSerializar
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import io 
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     data = Person.objects.all()
@@ -17,3 +21,20 @@ def details(request, pk):
     json_data = JSONRenderer().render(serializar.data)
 
     return HttpResponse(json_data)
+
+
+@csrf_exempt
+def create_person(request):
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        print('strem:', stream)
+        pythondata = JSONParser().parse(stream)
+        serializer = PersonSerializar(data = pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'successfully created instance !'}
+            js_data = JSONRenderer().render(res)
+            return HttpResponse(js_data, content_type = 'application/json')
+        json_data = JSONRenderer().render(serializar.errors)
+        return HttpResponse(js_data, content_type = 'application/json')
